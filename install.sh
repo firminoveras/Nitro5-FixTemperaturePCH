61b048b980765485908628e7a0cb5f46c12aa27b
Firmino Veras <firmino.veras@gmail.com>
2026-01-27 15:01:20 -0300

    change python to shell

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 yay -Syu
-yay -S nbfc-linux-git python
+yay -S nbfc-linux-git
 
 # Move files
-sudo cp pch_temp.py /usr/bin/
+sudo cp pch_temp_hammer.sh /usr/local/bin/
 sudo cp pch_temp.service /etc/systemd/system
+
+# Change exec 
+sudo chmod +x /usr/local/bin/pch_temp_hammer.sh
 
 # Enable service
 sudo systemctl daemon-reload
